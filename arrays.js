0b5049e5cd2db6e6daa3f340c8ca8953d503e794
Glenda Junker <glendajunker@hotmail.com>
2019-08-15 15:10:35 +0200

    Upload new file

---
+++ arrays.js
@@ -0,0 +1,9 @@
+// arrays are variables that you can store multiple values inside of them
+
+// to create an Array, it's recommended to give it a name that's plural.
+
+const schedule = ['Wake up', 'Eat', 'Film a video', 'Watch things on Netflix']; // the "[]"" basically says to JS that we want to create an array.
+
+const phoneNumbers = [5446841, 4945491, 78946545]
+
+console.log(schedule);
